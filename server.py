ab949d5ddbc5aa5fab84e8355bc076f375e9451f
tinaquelcraft <tinaquelcraft@gmail.com>
2024-07-10 03:52:54 +0000

    Mundo guardado 2024-07-10

--- server.py
+++ server.py
@@ -1,32 +1,3 @@
-# Versiones disponibles
-# forge, mohist, fabric, vanilla, paper
-
-# Puedes instalar mohist después de instalar forge desde el menú de gestionar
-# Puedes instalar paper después de instalar vanilla desde el menú de gestionar
-# Puedes instalar purpur después de instalar fabric desde el menú de gestionar
-
-# Regiones de ngrok
-# Código          Lugar
-#-----------      ---------------------------
-# ap	          Asia / Pacífico (Singapore)
-# au		      Australia (Sydney)
-# eu		      Europa (Frankfurt)
-# in		      India (Mumbai)
-# jp		      Japón (Tokyo)
-# sa		      Sudamérica (São Paulo)
-# us		      Estados unidos (Ohio)
-# us-cal-1	      Estados unidos (California)
-
-
-
-
-
-
-
-
-
-
-
 # No toques nada de aquí para abajo, puedes dañarlo
 import requests,os,base64,glob,time
 if os.path.exists("servidor.py"):
